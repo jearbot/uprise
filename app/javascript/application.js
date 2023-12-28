37a454facae265b05e67bf8bf7635e9eb4436f98
Jeremy Heskett <heskettjeremy@gmail.com>
2023-12-28 11:57:32 -0600

    commit

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Entry point for the build script in your package.json
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require_tree .
---
+++ app/models/student.rb
@@ -0,0 +1,13 @@
+class Student < ApplicationRecord
+  after_update :normalize_phone_number if phone_number_changed?
+
+  has_many :sms_messages
+
+  validates :phone_number, presence: true, uniqueness: true
+
+  private
+
+  def normalize_phone_number
+    self.update!(normalized_phone_number: PhonyRails.normalize_number(number, default_country_code: 'US'))
+  end
+end
