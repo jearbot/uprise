4e520990fa7e82610aa460c350825402c31f33a9
Jeremy Heskett <heskettjeremy@gmail.com>
2023-12-29 09:34:28 -0600

    commit

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,12 +2,8 @@ class MembersController < ApplicationController
   def index
     if params[:search].present?
       search_members
-    elsif params[:sort].present?
-      sort_column = params[:sort]
-      sort_direction = params[:sort_direction] || 'asc'
-      @members = Member.all.order("#{sort_column} #{sort_direction}").page(params[:page])
     else
-      @members = Member.all.page(params[:page])
+      @members = Member.all.order(name: :asc).page(params[:page])
     end
   end
 
@@ -19,17 +15,13 @@ class MembersController < ApplicationController
     search_term = params[:search].downcase if params[:search].present?
 
     @members = if Member.member_types.keys.include?(search_term)
-      Member.where(members_table[:member_type].eq(search_term))
-      .order(params[:sort] => sort_direction)
-      .page(params[:page])
-    else
-      Member.where(
-        members_table[:email].lower.matches("%#{search_term}%").
-        or(members_table[:normalized_phone_number].matches("%#{search_term}%")).
-        or(members_table[:name].lower.matches("%#{search_term}%"))
-      )
-      .order(params[:sort] => sort_direction)
-      .page(params[:page])
-    end
+                Member.where(members_table[:member_type].eq(search_term)).page(params[:page])
+               else
+                Member.where(
+                 members_table[:email].lower.matches("%#{search_term}%").
+                 or(members_table[:normalized_phone_number].matches("%#{search_term}%")).
+                 or(members_table[:name].lower.matches("%#{search_term}%"))
+                ).page(params[:page])
+               end
   end
 end
